ae97750a79614dc61ecfba3649a69de8845d53f7
cassiopeia <65559770+cassiopeian@users.noreply.github.com>
2021-08-28 22:29:49 -0400

    create .game-pieces on click function
    When the user clicks on a game piece, log the image's source to the variable gamePiece. If any div has the class .active applied, replace that div's (child) img's source with gamePiece and set the opacity to 1. After, strip the active class, so it doesn't get applied to more than one div at a time.

--- js/main.js
+++ js/main.js
@@ -3,4 +3,18 @@ $(document).ready(function() {
         // set the selected cell's background blue
         $(this).parent().addClass('active');
     });
+
+    $('.game-pieces').on('click', function() {
+        // log the source of the selected game piece
+        let gamePiece = $(this).attr('src');
+        
+        // if any grid cell is selected/active
+        if ($('div').hasClass('active') == true) {
+            // replace the active div's child img w/ the game piece
+            $('.active').children().attr('src', `${gamePiece}`).css('opacity', '1');
+
+            // ensure the active class only ever applies to one cell
+            $('.active').removeClass('active');
+        }
+    });
 });
