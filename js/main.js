8901044e882e0cbb3396800dc81694cbbc264f30
cassiopeia <65559770+cassiopeian@users.noreply.github.com>
2021-08-29 20:18:22 -0400

    create .subgrid div on click function
    This allows users to toggle through three possible background colors for each grid cell. Cells can be marked green, if the user is confident of their dessert choice; red, if they're unsure; or white, if they decide against using the markers.

--- js/main.js
+++ js/main.js
@@ -23,4 +23,21 @@ $(document).ready(function() {
             $('.active').addClass('no-aisu').removeClass('active');
         }
     });
+
+    $('.subgrid div').on('click', function() {
+        const green = 'rgb(224, 243, 237)';
+        const red = 'rgb(230, 163, 172)';
+        const white = 'rgb(255, 255, 255)';
+        const dessertCell = $(this).hasClass('no-aisu');
+        const cellColor = $(this).css('backgroundColor');
+
+        // if cells are populated, toggle through 3 bg colors
+        if (dessertCell == true && cellColor == white) {
+            $(this).css('backgroundColor', green);
+        } else if (dessertCell == true && cellColor == green) {
+            $(this).css('backgroundColor', red);
+        } else if (dessertCell == true && cellColor == red) {
+            $(this).css('backgroundColor', white);
+        }
+    });
 });
