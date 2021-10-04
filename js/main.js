27331ff252f04b7b9da7ab3262ad3d0712064245
cassiopeia <65559770+cassiopeian@users.noreply.github.com>
2021-10-03 22:53:42 -0400

    delete updateMenuBoard()
    Decided not to have two menu board shapes.

--- js/main.js
+++ js/main.js
@@ -41,23 +41,7 @@ $(document).ready(function() {
     
     setGridHeight();
 
-    function updateMenuBoard() {
-        // at the max-width: 1050px breakpoint
-        if ($(window).width() < 1051) {
-            // replace the square menu with the rectangular one
-            $('#menu-board').attr('src', 'images/menu/rectangular-menu.svg');
-        } else {
-            // otherwise, revert back to the square
-            $('#menu-board').attr('src', 'images/menu/square-menu.svg');
-        }
-    }
-
-    updateMenuBoard();
-
-    $(window).on('resize', function() {
-        setGridHeight();
-        updateMenuBoard();
-    });
+    $(window).on('resize', setGridHeight);
     
     // set the populated cells apart from the others
     $('.populated-cell').parent().css('backgroundColor', 'rgb(240, 248, 255)');
